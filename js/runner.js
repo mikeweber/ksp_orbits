160a4450a1c60564043182a1e88d5e9e7e500019
Mike Weber <mike.weber@govdelivery.com>
2015-11-09 14:39:45 -0600

    Add hints to buttons

--- js/runner.js
+++ js/runner.js
@@ -1,5 +1,5 @@
 var sun       = new window.Sun(1.1723328e18, 2.616e8),
-    kerbin    = new window.Planet(sun, 6e5,   '#33F', 3.5316e12,    9284.5, 13599840256, { r: 13599840256, phi: -Math.PI }, 0,    Math.PI / 2, 8.4159286e7),
+    kerbin    = new window.Planet(sun, 6e5,   '#77F', 3.5316e12,    9284.5, 13599840256, { r: 13599840256, phi: -Math.PI }, 0,    Math.PI / 2, 8.4159286e7),
     duna      = new window.Planet(sun, 3.2e5, '#F33', 3.0136321e11, 7915,   20726155264, { r: 19669121365, phi: -Math.PI }, 0.05, Math.PI / 2, 4.7921949e7),
     ship1_man = [[1.907e7, { heading: Math.PI }], [1.924e7, { heading: -Math.PI / 2 }], [1.93e7, null]],
     launches  = [
@@ -17,7 +17,7 @@ var sun       = new window.Sun(1.1723328e18, 2.616e8),
     canvas    = { width: 500, height: 500 },
     renderer  = new window.Renderer(document.getElementById('flightplan'), world, canvas),
     t         = 1.88719e7,
-    s         = new window.Simulator(t, [sun, duna, kerbin], 1)
+    s         = new window.Simulator(t, [sun, duna, kerbin], 100)
 s.track(kerbin)
 renderer.zoomTo(1)
 s.run(renderer)
@@ -25,17 +25,19 @@ for (var i = launches.length; i--; ) {
   s.registerShipLaunch(launches[i])
 }
 function startListeners(sim, renderer) {
-  document.getElementById('pause').addEventListener('click',    function() { sim.togglePaused(renderer) })
-  document.getElementById('zoom_in').addEventListener('click',  renderer.zoomIn.bind(renderer))
-  document.getElementById('zoom_out').addEventListener('click', renderer.zoomOut.bind(renderer))
-  document.getElementById('faster').addEventListener('click',   sim.faster.bind(sim))
-  document.getElementById('slower').addEventListener('click',   sim.slower.bind(sim))
+  document.getElementById('pause').addEventListener('click',       function() { sim.togglePaused(renderer) })
+  document.getElementById('zoom_in').addEventListener('click',     renderer.zoomIn.bind(renderer))
+  document.getElementById('zoom_out').addEventListener('click',    renderer.zoomOut.bind(renderer))
+  document.getElementById('faster').addEventListener('click',      sim.faster.bind(sim))
+  document.getElementById('slower').addEventListener('click',      sim.slower.bind(sim))
+  document.getElementById('prev_target').addEventListener('click', sim.trackPrev.bind(sim))
+  document.getElementById('next_target').addEventListener('click', sim.trackNext.bind(sim))
   window.onkeyup = function(e) {
     var key = e.keyCode ? e.keyCode : e.which
 
     if (key === 27 || key === 32) {
       // esc
-      s.togglePaused(renderer)
+      sim.togglePaused(renderer)
     } else if (key === 187) {
       // +
       renderer.zoomIn()
@@ -44,16 +46,16 @@ function startListeners(sim, renderer) {
       renderer.zoomOut()
     } else if (key === 190) {
       // >
-      s.faster()
+      sim.faster()
     } else if (key === 188) {
       // <
-      s.slower()
+      sim.slower()
     } else if (key === 221) {
       // [
-      s.trackNext()
+      sim.trackNext()
     } else if (key === 219) {
       // ]
-      s.trackPrev()
+      sim.trackPrev()
     }
   }
 }
