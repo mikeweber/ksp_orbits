bb7b731101a170ad0d01baf32d37ec1a9f19599b
Mike Weber <mike@weberapps.com>
2015-12-15 09:13:12 -0600

    Add another flight plan

--- js/runner.js
+++ js/runner.js
@@ -1,23 +1,31 @@
 /* globals FlightPlanner Decimal jQuery */
 
+var start_time = 0
+// for Duna Intercept
+//start_time = 1.88719e7
+
+// for Duna approach
+start_time = 1.9252e7
+
+// for Duna return
+//start_time = null
 var player = initUniverse()
 player.zoomTo(new Decimal(200))
 player.run()
-player.track('Ike')
 addListeners(jQuery, player)
-runDunaIntercept(player, jQuery)
+//runDunaIntercept(player, jQuery)
 //runFlightBack(player, jQuery)
+runDunaApproach(player, jQuery)
 
-function followShipAndTarget(ship, player) {
+function followShipAndTarget(ship, final_target, player) {
   'use strict'
 
   var zoom, coords1, coords3, zoom_x, zoom_y, dist_x, dist_y, dist_x2, dist_y2, port_x, port_y,
       closest_zoom = 500,
-      kerbin       = player.sim.getBody('Kerbin'),
-      coords2      = kerbin.getCoordinates(),
-      duna         = player.sim.getBody('Duna'),
+      coords2      = ship.getCoordinates(),
       cur_target   = { getCoordinates: function() { return coords2 } }
 
+  player.renderer.track(ship)
   function follow() {
     coords1 = ship.getCoordinates()
     coords2 = cur_target.getCoordinates()
@@ -25,12 +33,12 @@ function followShipAndTarget(ship, player) {
     port_y  = player.renderer.getViewportY().times(0.8)
     dist_x  = coords1.x.minus(coords2.x).abs()
     dist_y  = coords1.y.minus(coords2.y).abs()
-    if (cur_target !== duna) {
-      coords3 = duna.getCoordinates()
+    if (cur_target !== final_target) {
+      coords3 = final_target.getCoordinates()
       dist_x2 = coords1.x.minus(coords3.x).abs()
       dist_y2 = coords1.y.minus(coords3.y).abs()
       if (dist_x2.lt(port_x) || dist_y2.lt(port_y)) {
-        cur_target = duna
+        cur_target = final_target
         dist_x     = dist_x2
         dist_y     = dist_y2
       }
@@ -47,38 +55,6 @@ function followShipAndTarget(ship, player) {
   player.observe('after:smoothZoomOut', function() { player.renderer.unobserve('before:render', follow) })
   player.observe('after:trackNext',     function() { player.renderer.unobserve('before:render', follow) })
   player.observe('after:trackPrev',     function() { player.renderer.unobserve('before:render', follow) })
-}
-
-function runFlightBack(player, $) {
-  'use strict'
-
-  var stat = new FlightPlanner.View.FlightStatus(sim, 1, 'Launching from Duna')
-  $('#status').append(stat.getPanel())
-
-  sim.removeBody('Duna Mission')
-  var plan = new FlightPlanner.Model.FlightPlan(player, 'Return Mission', stat, 18872150).scheduleLaunchFromPlanet(
-    sim.getBody('Duna'),
-    2200,
-    {
-      throttle:         1,
-      max_accel:        0.2,
-      fuel_consumption: 0.19,
-      initial_angle:    Math.PI / 2,
-      heading:          0,
-      absolute_heading: true,
-      target:           sim.getBody('Kerbin')
-    }
-  )
-  function addShipRenderer(ship) {
-    var ship_r  = new FlightPlanner.View.PlanetRenderer(ships, ship, '#FFFFFF', 2),
-        ship_cr = new FlightPlanner.View.ConicRenderer(conics, ship)
-    player.renderer.registerRenderer(ship_r)
-    player.renderer.registerRenderer(ship_cr)
-    followShipAndTarget(ship, player)
-    plan.unobserve('after:blastOff', addShipRenderer)
-  }
-
-  plan.observe('after:blastOff', addShipRenderer)
 }
 
 function initUniverse() {
@@ -111,9 +87,7 @@ function initUniverse() {
       canvas_dimensions = { width: 700, height: 700 },
       bg        = $('#background')[0],
       renderer  = new FlightPlanner.View.Renderer(canvas, world, canvas_dimensions),
-      launch_t  = 1.88719e7,
-      return_t  = 1.887215e7,
-      sim       = new FlightPlanner.Controller.Simulator(launch_t, [ike, duna, minmus, mun, kerbin, sun], 64),
+      sim       = new FlightPlanner.Controller.Simulator(start_time, [ike, duna, minmus, mun, kerbin, sun], 64),
       player    = new FlightPlanner.Controller.Player(sim, renderer)
 
   kerbin.addChild(mun)
@@ -137,6 +111,41 @@ function initUniverse() {
   renderer.registerRenderer(ike_r)
 
   return player
+}
+
+function runFlightBack(player, $) {
+  'use strict'
+
+  var stat = new FlightPlanner.View.FlightStatus(player.sim, 1, 'Launching from Duna')
+  $('#status').append(stat.getPanel())
+
+  player.sim.removeBody('Duna Mission')
+  var plan = new FlightPlanner.Model.FlightPlan(player, 'Return Mission', stat, 18872150).scheduleLaunchFromPlanet(
+    sim.getBody('Duna'),
+    2200,
+    {
+      throttle:         1,
+      max_accel:        0.2,
+      fuel_consumption: 0.19,
+      initial_angle:    Math.PI / 2,
+      heading:          0,
+      absolute_heading: true,
+      target:           sim.getBody('Kerbin')
+    }
+  )
+
+  plan.observe('after:blastOff', addShipRenderer)
+  plan.observe('after:blastOff', function(ship) {
+    followShipAndTarget(ship, player.sim.getBody('Kerbin'), player)
+  })
+
+  function addShipRenderer(ship) {
+    var ship_r  = new FlightPlanner.View.PlanetRenderer(ships, ship, '#FFFFFF', 2),
+        ship_cr = new FlightPlanner.View.ConicRenderer($('#flightpaths')[0], ship)
+    player.renderer.registerRenderer(ship_r)
+    player.renderer.registerRenderer(ship_cr)
+    plan.unobserve('after:blastOff', addShipRenderer)
+  }
 }
 
 function runDunaIntercept(player, $) {
@@ -164,11 +173,94 @@ function runDunaIntercept(player, $) {
         ship_cr = new FlightPlanner.View.ConicRenderer($('#flightpaths')[0], ship)
     player.renderer.registerRenderer(ship_r)
     player.renderer.registerRenderer(ship_cr)
-    followShipAndTarget(ship, player)
     plan.unobserve('after:blastOff', addShipRenderer)
   }
 
   plan.observe('after:blastOff', addShipRenderer)
+  plan.observe('after:blastOff', function(ship) {
+    followShipAndTarget(ship, player.sim.getBody('Duna'), player)
+  })
+
+  plan.addManeuver(function(t, ship) { return ship.getMissionTime(t).greaterThan(2.01e5) }, Math.PI, false, 1).done(function(status_tracker) {
+    status_tracker.setMessage('Decelerating on approach to Duna')
+  })
+  plan.addManeuver(function(t, ship) { return ship.getMissionTime(t).greaterThan(3.88e5) }, player.sim.getBody('Duna').getPrograde().plus('' + (-Math.PI * 0.45)), true, 1).done(function(status_tracker) {
+    status_tracker.setMessage('Matching velocity and vector with planet')
+  })
+  plan.addManeuver(function(t, ship) { return player.sim.t.greaterThan(1.9252e7) }, 0, false, 0).done(function(status_tracker) {
+    player.togglePaused()
+  })
+  plan.addManeuver(function(t, ship) { return ship.getMissionTime(t).greaterThan(4.18e5) }, 0, false, 0).done(function(status_tracker) {
+    status_tracker.setMessage('Waiting for intercept...')
+  })
+  plan.addSOIChangeManeuver(player.sim.getBody('Kerbol'), Math.PI * 0.727, true, 1).done(function(status_tracker) {
+    status_tracker.setMessage('Left Kerbin SOI; setting course')
+  })
+  plan.addSOIChangeManeuver(player.sim.getBody('Duna'), Math.PI, false, 1).done(function(status_tracker, ship, t) {
+    var i, soi_change_time = t
+    status_tracker.setMessage('Near destination; attempting to get captured')
+
+    plan.addManeuver(function(t, ship) { return ship.getVelocity().lt(ship.parent.mu.times(new Decimal(1).dividedBy(ship.pos.r)).sqrt()) }, 0, false, 0).done(function(status_tracker) {
+      status_tracker.setMessage('Capture complete; Shutting down engines')
+
+      plan.addManeuver(function(t, ship) { return ship.pos.r.minus(500).lt(ship.calcOrbitalParams().pe) }, -Math.PI, false, 1).done(function(status_tracker) {
+        status_tracker.setMessage('Lowering periapsis and circularizing orbit')
+
+        plan.addManeuver(function(t, ship) { return ship.calcOrbitalParams().pe.lt(500000) }, 0, false, 0).done(function(status_tracker) {
+          status_tracker.setMessage('Waiting to reach parking orbit')
+
+          plan.addManeuver(function(t, ship) { return ship.pos.r.minus(35000).lt(ship.calcOrbitalParams().pe) }, -Math.PI, false, 1).done(function(status_tracker) {
+            status_tracker.setMessage('Circularizing orbit...')
+
+            // Circular orbit at 600km from center of Duna has a velocity of 818 m/s
+            plan.addManeuver(function(t, ship) { return ship.getVelocity().lt(820) }, 0, false, 0).done(function(status_tracker) {
+              status_tracker.setMessage('Parking orbit reached')
+            })
+          })
+        })
+      })
+    })
+  })
+}
+
+function runDunaApproach(player, $) {
+  'use strict'
+
+  var stat = new FlightPlanner.View.FlightStatus(player.sim, 1, 'Approaching Duna')
+  $('#status').append(stat.getPanel())
+
+  var plan = new FlightPlanner.Model.FlightPlan(player, 'Duna Mission', stat, 19252100).placeShip(
+    player.sim.getBody('Kerbol'),
+    9348,
+    { r: 21288152000, phi: 141 / 180 * Math.PI },
+    116 / 180 * Math.PI,
+    {
+      throttle:         0,
+      max_accel:        0.2,
+      fuel_consumption: 0.19,
+      heading:          -Math.PI,
+      absolute_heading: false,
+      target:           player.sim.getBody('Duna'),
+      mission_time:     376384
+    }
+  )
+
+  function addShipRenderer(ship) {
+    var ship_r  = new FlightPlanner.View.PlanetRenderer(ships, ship, '#FFFFFF', 2),
+        ship_cr = new FlightPlanner.View.ConicRenderer($('#flightpaths')[0], ship)
+    player.renderer.registerRenderer(ship_r)
+    player.renderer.registerRenderer(ship_cr)
+    plan.unobserve('after:blastOff', addShipRenderer)
+  }
+
+  plan.observe('after:blastOff', addShipRenderer)
+  plan.observe('after:blastOff', function() {
+    player.renderer.observe('after:render', function() {
+    })
+  })
+  plan.observe('after:blastOff', function(ship) {
+    followShipAndTarget(ship, player.sim.getBody('Duna'), player)
+  })
   plan.addManeuver(function(t, ship) { return ship.getMissionTime(t).greaterThan(2.01e5) }, Math.PI, false, 1).done(function(status_tracker) {
     status_tracker.setMessage('Decelerating on approach to Duna')
   })
