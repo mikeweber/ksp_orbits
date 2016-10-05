00b16c9877cd99337dbd50ea44b1fd3cf4e03bc9
Mike Weber <mike@weberapps.com>
2016-10-05 18:19:52 -0500

    Change target orbit to 100km (300km from Mun center)

--- js/mun_mission.js
+++ js/mun_mission.js
@@ -13,9 +13,9 @@ var mun_mission = runMunIntercept(player, 'Mün Mission', launch_time, jQuery)
 function runMunIntercept(player, name, launch_time, $) {
   'use strict'
 
-  
-  var initial_angle = 31.15 // 32 hits mun dead on
-  var turn_around_time = 6260
+  // ship position with launch phase angle of -31.8 and about face as 6290 --  (Kerbin, 50, 2119.2626792253828821, { r: 6816444.2806136252258, phi: -2.66610616214890326 }, 0.20829248466088174, 3.7685509195579044, false)
+  var initial_angle = 30.5 // 32 hits mun dead on
+  var turn_around_time = 6270
   var stat = new FlightPlanner.View.FlightStatus(player.sim, 1, 'Launching from Kerbin'),
       mun = player.sim.getBody('Mün'),
       kerbin = player.sim.getBody('Kerbin')
@@ -65,13 +65,19 @@ function runMunIntercept(player, name, launch_time, $) {
     logger.logShipTelemetry(ship, t, msg)
     status_tracker.setMessage(msg)
   })
+  
+  var log_position_before_soi_change = plan.addManeuver(function(t, ship) { return ship.pos.r.gt(9.5e6) } , Math.PI, false, 1).done(function(status_tracker, ship, t) {
+    var msg = 'Logging position.'
+    logger.logShipTelemetry(ship, t, msg)
+    status_tracker.setMessage(msg)
+  })
 
   var hit_mun = plan.addSOIChangeManeuver(mun, Math.PI, false, 1).done(function(status_tracker, ship, t) {
-    var msg = 'Entered Mün\'s SOI'
+    var msg = 'Entered Mün\'s SOI.'
     logger.logShipTelemetry(ship, t, msg)
     status_tracker.setMessage(msg)
 
-    var getCaptured = plan.addManeuver(function(t, ship) { return ship.getApoapsis().gt(0) && ship.getApoapsis().lt(2.0e6) && ship.getPeriapsis().lt(2.5e5) }, 0, false, 0).done(function(status_tracker, ship, t) {
+    var getCaptured = plan.addManeuver(function(t, ship) { return ship.getApoapsis().gt(0) && ship.getApoapsis().lt(2.0e6) && ship.getPeriapsis().lt(3.0e5) }, Math.PI, false, 1).done(function(status_tracker, ship, t) {
       msg = 'captured'
       logger.logShipTelemetry(ship, t, msg)
       status_tracker.setMessage(msg)
