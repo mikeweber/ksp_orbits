44e1de3ac1b75c99fbdf39e08049ab90ba21c306
Mike Weber <mike.weber@govdelivery.com>
2016-10-07 16:53:08 -0500

    Show prograde direction in flight log

--- js/mun_mission.js
+++ js/mun_mission.js
@@ -1,7 +1,7 @@
 /* globals FlightPlanner Decimal jQuery */
 
 // for Duna Intercept
-var launch_time = 3 * DAY + 4 * HOUR
+var launch_time = 3 * DAY + 4 * HOUR + 5 * MIN + 30
 var start_time = launch_time - 1
 
 var player = FlightPlanner.Sim.initUniverse(start_time)
@@ -13,8 +13,8 @@ var mun_mission = runMunIntercept(player, 'Mün Mission', launch_time, jQuery)
 function runMunIntercept(player, name, launch_time, $) {
   'use strict'
 
-  var initial_angle = 30.55
-  var turn_around_time = 6255
+  var initial_angle = 31.4
+  var turn_around_time = 6273
   // 85400.1 (t+6200): Mün Mission - (Kerbin, 50, 2097.0692117476370058, { r: 6527565.074370169754, phi: -2.67276385110575904 }, 0.18159158606929396, 0.6176410486172988, false) -- initial_angle: 30.55
   var stat = new FlightPlanner.View.FlightStatus(player.sim, 1, 'Launching from Kerbin'),
       mun = player.sim.getBody('Mün'),
